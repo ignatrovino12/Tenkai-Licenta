f423fa50d1c591c0e88af943b02dca0d191fce00
ignatrovino12 <ignatrovino12@gmail.com>
2024-06-22 14:10:06 +0000

    secret

--- licenta_site/licenta_site/settings.py
+++ licenta_site/licenta_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_33$r_96c37pspc%*ia=q*o2%xo3tp2a*rc1ft#llw35ih%f)1"
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
